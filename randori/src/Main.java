3f28556d2fd188be6d373d502a95b8797511a513
Pocaklako <ccrolcsi@gmail.com>
2020-05-12 10:35:38 +0200

    halfdone

--- randori/src/Main.java
+++ randori/src/Main.java
@@ -12,12 +12,13 @@ public class Main {
     public static void main(String[] args) {
         String[] x = text.split("\\W+");
 
-
-        for (int i = 0; i < x.length; i++) {
-            if (i == 0) System.out.println("====================");
-            if (i % 2 == 1) System.out.println(x[i]);
+        System.out.println("====================");
+        for (int i = 0; i < x.length; i = i+2) {
+            //if (i % 2 == 1) System.out.println(x[i]);
+            System.out.printf("%-14s %03d %n", x[i], Integer.parseInt(x[i+1]));
 
         }
+        System.out.println("====================");
 
         System.out.println(Arrays.toString(x));
     }
