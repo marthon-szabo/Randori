0e0a8e39d815b82f983198270ab78142921da56a
marthon-szabo <makotoimx@gmail.com>
2020-05-12 10:55:43 +0200

    Add iteration

--- randori/src/Main.java
+++ randori/src/Main.java
@@ -1,25 +1,28 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static String text = "java 100\ncpp 65\npython 50";
-
-
+    public static String[] words = {"5","Goodbye bye bye world world world",
+            "Sam went went to to to his business",
+            "Reya is is the the best player in eye eye game",
+            "in inthe",
+            "Hello hello Ab aB"};
     public static void listBuilder(String text){
 
     }
 
     public static void main(String[] args) {
-        String[] x = text.split("\\W+");
+        List<String> x = new ArrayList<>();
 
-        System.out.println("====================");
-        for (int i = 0; i < x.length; i = i+2) {
-            //if (i % 2 == 1) System.out.println(x[i]);
-            System.out.printf("%-14s %03d %n", x[i], Integer.parseInt(x[i+1]));
+        for (int i = 1; i < words.length; i++) {
+            for (int j = 0; j < i.length(); j++) {
 
+            }
         }
-        System.out.println("====================");
 
-        System.out.println(Arrays.toString(x));
+
     }
 }
