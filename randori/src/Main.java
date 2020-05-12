f1b3a5fe99839d67a6a57e5b3473755a36d2b0ec
marthon-szabo <makotoimx@gmail.com>
2020-05-12 10:16:35 +0200

    Add text split

--- randori/src/Main.java
+++ randori/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static String text = "java 100\ncpp 65\npython 50";
@@ -9,14 +11,9 @@ public class Main {
 
 
 
-
     public static void main(String[] args) {
-        String[] x = text.split(" \n");
+        String[] x = text.split("\\W+");
 
-
-
-
-
-
+        System.out.println(Arrays.toString(x));
     }
 }
