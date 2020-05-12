967efd99bb354309ffe554003cadcaefb0b4d1b1
Tóth Máté <tothmate911@gmail.com>
2020-05-12 10:28:19 +0200

    x

--- randori/src/Main.java
+++ randori/src/Main.java
@@ -9,10 +9,15 @@ public class Main {
 
     }
 
-
-
     public static void main(String[] args) {
         String[] x = text.split("\\W+");
+
+
+        for (int i = 0; i < x.length; i++) {
+            if (i == 0) System.out.println("====================");
+            if (i % 2 == 1) System.out.println(x[i]);
+
+        }
 
         System.out.println(Arrays.toString(x));
     }
