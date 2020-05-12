9e7499811abffaeadbc49de18c6b91af934f9ef3
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-05-12 10:01:33 +0200

    A

---
+++ randori/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static String text = "java 100\ncpp 65\npython 50";
+
+
+    public static void listBuilder(String text){
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        String[] x = text.split(" \n");
+
+
+
+
+
+
+    }
+}
